5e520b6ebe5e27f67fb830e77ea6f2ee9c377c2c
Ad Soyad <mhmmdadgzl@outlook.com>
2026-01-29 14:07:42 +0300

    Remove HOST var

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,6 @@
 export SHOPIFY_API_KEY=d5568d43c70e94118794afd517b5d8ef
 export SHOPIFY_API_SECRET=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
 export SCOPES=write_products,read_products,write_orders,read_orders
-export HOST=https://ssaw-e.techifyboost.com
 export SHOPIFY_APP_URL=https://ssaw-e.techifyboost.com
 export SSACTIVEWEAR_USER=your_user_id
 export SSACTIVEWEAR_KEY=your_api_key
